569d8bef007f861c84cfb5f57453b6bc8a7768e6
Anshul-Dishoriya <anshuldishoriya@gmail.com>
2023-10-16 12:31:44 +0530

    leftnav func fixed

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -23,8 +23,9 @@ const LeftNav = () => {
         break;
     }
   };
+  console.log(mobileMenu)
   return (
-    <div className={`${mobileMenu ? 'translate-x-0' : ''} md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all`}>
+    <div className={`${mobileMenu ? 'translate-x-1' : ''} md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all`}>
       <div className="flex px-5 flex-col">
         {categories.map((item) => {
           return (
